0738324e3837f56fba70a40bddb7413c083bd338
Phyllis Kan <pyk9q7@gmail.com>
2019-07-04 23:16:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,30 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+   while i < array.length
+      yield array[i]
+      i+=1
+   end
+   array
+end
+
+def my_collect(array)
+   i = 0
+   new = []
+   while i < array.length
+      new << yield(array[i])
+      i += 1
+   end
+   new
+end
+
+def my_select(collection)
+   i = 0
+   new = []
+   while i < collection.length
+      if yield(collection[i]) == true
+         new << collection[i]
+      end
+      i += 1
+   end
+   new
 end
